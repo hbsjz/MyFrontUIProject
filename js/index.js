183ff2a032ef251eee6f581bb80f6ddd81ca6f29
unknown <664008173@qq.com>
2019-06-16 20:48:26 +0800

    16提交

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(function(){
+    // 设置右上菜单监听
+    $("#about_us").hover(function(){
+        
+        $("#item_ul_right_menu").toggleClass("hidde_menu");
+    });
+    
+    
+
+
+
+
+
+
+
+
+
+})
