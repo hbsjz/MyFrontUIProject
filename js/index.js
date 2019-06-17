6e491439aefd28c4f1f61b395aa13770aa0e295f
unknown <664008173@qq.com>
2019-06-17 22:54:23 +0800

    2019-06-17晚上家提交

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(function(){
     // 设置右上菜单监听
-    $("#about_us").hover(function(){
-        
+    $("#right_nav>ul>li").hover(function(){
+        // console.log($(this));
         $("#item_ul_right_menu").toggleClass("hidde_menu");
     });
     
