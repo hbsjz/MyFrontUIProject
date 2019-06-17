b1f20b87cd25976d4f56b4417b4a3a4d73877145
unknown <664008173@qq.com>
2019-06-17 23:35:56 +0800

    li的改动

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 $(function(){
     // 设置右上菜单监听
     $("#right_nav>ul>li").hover(function(){
-        // console.log($(this));
-        $("#item_ul_right_menu").toggleClass("hidde_menu");
+        console.log($(this));
+       
+        // $("#item_ul_right_menu").toggleClass("hidde_menu");
     });
     
     
