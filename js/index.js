637021f6dd8d70978d4959f5ac1b2f742e18250a
unknown <664008173@qq.com>
2019-06-19 17:31:14 +0800

    19日提交

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
-$(function(){
+$(function () {
+    //js中添加数据
+
+
+
+
     // 设置右上菜单监听
-    $("#right_nav>ul>li").hover(function(){
-        console.log($(this));
-       
-        // $("#item_ul_right_menu").toggleClass("hidde_menu");
+    $("#right_nav>ul>li").hover(function () {
+        $(this).children("#item_ul_right_menu").stop();
+        $(this).children("#item_ul_right_menu").slideToggle(300);
+
     });
-    
-    
+
+
 
 
 
