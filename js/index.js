89131992c4d1eb88c59d40f88d3300c595ff1148
unknown <664008173@qq.com>
2019-06-28 15:38:26 +0800

    id 替换成class

--- js/index.js
+++ js/index.js
@@ -5,9 +5,9 @@ $(function () {
 
 
     // 设置右上菜单监听
-    $("#right_nav>ul>li").hover(function () {
-        $(this).children("#item_ul_right_menu").stop();
-        $(this).children("#item_ul_right_menu").slideToggle(300);
+    $(".right_nav>ul>li").hover(function () {
+        $(this).children(".item_ul_right_menu").stop();
+        $(this).children(".item_ul_right_menu").slideToggle(300);
     });
 
 
