01909f163dd69a4ebfbf95eba8e373f48694cc79
unknown <664008173@qq.com>
2019-07-05 10:46:25 +0800

    7月5日

--- js/login.js
+++ js/login.js
@@ -2,7 +2,14 @@ $(function(){
     $(".loginButton").click(function (e) { 
     var userName=$("#usernameinput").val();
     var password=$("#passwordinput").val();
-    console.log("用户名"+userName+"----密码："+password);
+    
+    if(userName&&password){
+        console.log("用户名"+userName+"----密码："+password);
+    }else{
+        console.log("用户名"+userName+"----密码："+password);
+        toastr.warning("密码用户名不能为空");
+        return;
+    }
     
 
        $.getJSON("http://192.168.1.106:8080/loginMy?username="+userName+"&password="+password, "",
