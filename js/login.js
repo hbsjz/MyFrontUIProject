3fa9a77d7fdca8d77741aa92b5e67355653e9e5e
18932540203 <Lin630790liN>
2019-07-08 16:53:26 +0800

    8日提交

--- js/login.js
+++ js/login.js
@@ -1,23 +1,50 @@
-$(function(){
-    $(".loginButton").click(function (e) { 
-    var userName=$("#usernameinput").val();
-    var password=$("#passwordinput").val();
-    
-    if(userName&&password){
-        console.log("用户名"+userName+"----密码："+password);
-    }else{
-        console.log("用户名"+userName+"----密码："+password);
-        toastr.warning("密码用户名不能为空");
-        return;
-    }
-    
+$(function () {
 
-       $.getJSON("http://192.168.1.106:8080/loginMy?username="+userName+"&password="+password, "",
-           function (data, textStatus, jqXHR) {
-               console.log(data);
-               
-           }
-       );
-    
+    optionsTost();
+
+
+    $(".loginButton").click(function (e) {
+        var userName = $("#usernameinput").val();
+        var password = $("#passwordinput").val();
+
+        if (userName && password) {
+            console.log("用户名" + userName + "----密码：" + password);
+        } else {
+            console.log("用户名" + userName + "----密码：" + password);
+            toastr.warning("密码用户名不能为空");
+            return;
+        }
+        var dataMap=new Map();
+        dataMap.set("username","admin");
+        dataMap.set("password","mima");
+        dataMap.set("role","role");
+
+        $.post("http://192.168.1.114:8080/zlq_war_exploded/login", dataMap,
+            function (data, textStatus, jqXHR) {
+
+            },
+            "dataType"
+        );
+
     });
+
+
+    //定义tost的参数
+    function optionsTost() {
+        toastr.options = {
+            closeButton: false,
+            debug: false,
+            progressBar: false,
+            positionClass: "toast-bottom-center",
+            onclick: null,
+            showDuration: "300",
+            hideDuration: "1000",
+            timeOut: "2000",
+            extendedTimeOut: "1000",
+            showEasing: "swing",
+            hideEasing: "linear",
+            showMethod: "fadeIn",
+            hideMethod: "fadeOut"
+        };
+    };
 })
