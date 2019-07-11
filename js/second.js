bd0c77e652407ac7f7cbd42ec6559708c7984b13
18932540203 <Lin630790liN>
2019-07-11 17:17:03 +0800

    11日提交

--- js/second.js
+++ js/second.js
@@ -10,13 +10,6 @@ $(function(){
         
     });
 
-    function GetQueryString(name)
-{
-     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
-     var r = window.location.search.substr(1).match(reg);
-     if(r!=null)return  unescape(r[2]); return null;
-}
-
 
  function getParam(paramName){  
        paramValue = "", isFound = !1;  
